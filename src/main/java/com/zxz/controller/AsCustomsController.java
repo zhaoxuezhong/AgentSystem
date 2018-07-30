280e05a2ae5c6cb00ff93d3f09998075319440d8
zhaoxuezhong <13268024463@163.com>
2018-07-30 21:15:04 +0800

    test

---
+++ src/main/java/com/zxz/controller/AsCustomsController.java
@@ -0,0 +1,15 @@
+package com.zxz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author zhaoxuezhong
+ *	AsCustomsController
+ */
+@Controller
+@RequestMapping(value="agent/customs")
+public class AsCustomsController {
+
+
+}
